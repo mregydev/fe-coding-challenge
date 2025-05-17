93f7f1098e66cab99cc9296b0958f68fa908b9b6
mregydev <mregydev@gmail.com>
2025-05-17 02:35:02 +0200

    chore:adding speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from "next";
 import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -20,10 +21,11 @@ export default function RootLayout({children,}: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+      <html lang='en'>
         <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+          {children}
+          <SpeedInsights />
         </body>
-        </html>
+      </html>
     );
 }
