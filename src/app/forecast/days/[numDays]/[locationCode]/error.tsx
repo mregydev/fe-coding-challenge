c96de5caa665183c95bc7dcc7a0d2ff91e7b68e0
mregydev <mregydev@gmail.com>
2025-05-17 02:40:17 +0200

    chore:removing error props from error page

--- src/app/forecast/days/[numDays]/[locationCode]/error.tsx
+++ src/app/forecast/days/[numDays]/[locationCode]/error.tsx
@@ -1,21 +1,19 @@
 'use client';
 
 export interface ErrorPageProps {
-  error: Error;
   reset: () => void;
 }
 
-export default function Error({ error, reset }: ErrorPageProps) {
-  
+export default function Error({ reset }: ErrorPageProps) {
   return (
-    <div className="flex flex-col items-center justify-center min-h-[60vh] text-center px-4">
-      <h2 className="text-2xl font-semibold mb-2">Something went wrong</h2>
-      <p className="text-gray-600 mb-4">
+    <div className='flex flex-col items-center justify-center min-h-[60vh] text-center px-4'>
+      <h2 className='text-2xl font-semibold mb-2'>Something went wrong</h2>
+      <p className='text-gray-600 mb-4'>
         We couldn’t load the weather forecast. Please try again later.
       </p>
       <button
         onClick={() => reset()}
-        className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition"
+        className='px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition'
       >
         Try again
       </button>
